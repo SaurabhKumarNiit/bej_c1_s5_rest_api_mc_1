8dc60afaeb5ae3b6d07981464516b91c86e07a4b
Saurabh Kumar Tiwari <sky.royaltiwari@gmail.com>
2022-11-02 17:29:39 +0530

    done

---
+++ src/main/java/com/example/restapi/demoRestApi/service/MovieMainService.java
@@ -0,0 +1,12 @@
+package com.example.restapi.demoRestApi.service;
+
+import com.example.restapi.demoRestApi.domain.MovieService;
+
+import java.util.List;
+
+public interface MovieMainService {
+
+    public MovieService addMovies(MovieService movieService);
+
+    public List<MovieService> getAllUsers();
+}
